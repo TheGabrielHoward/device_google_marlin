01bd4d3e3d9fcb887221ddaafe08fda78954f5f7
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-18 21:58:09 +0100

    IMS Support
    Build what we can
    
    Change-Id: I0c46469ed7dadb0ac56995accb87faac76dceead
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- device-bootleg.mk
+++ device-bootleg.mk
@@ -2,6 +2,13 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1
 
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager.xml \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
+
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
