605d52b667eb29e1815f61d18a005e769b8e6914
shagbag913 <sh4gbag913@gmail.com>
2019-02-04 20:41:00 -0500

    marlin/sailfish: PQ2A.190205.003
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- bootleg_marlin.mk
+++ bootleg_marlin.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 9 PQ1A.190105.004 5148680 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 9 PQ2A.190205.003 5180536 release-keys"
 
-BUILD_FINGERPRINT := google/marlin/marlin:9/PQ1A.190105.004/5148680:user/release-keys
+BUILD_FINGERPRINT := google/marlin/marlin:9/PQ2A.190205.003/5180536:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bootleggers.maintainer=stebomurkn420
--- bootleg_sailfish.mk
+++ bootleg_sailfish.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 9 PQ1A.190105.004 5148680 release-keys"
+    PRIVATE_BUILD_DESC="sailfish-user 9 PQ2A.190205.003 5180536 release-keys"
 
-BUILD_FINGERPRINT := google/sailfish/sailfish:9/PQ1A.190105.004/5148680:user/release-keys
+BUILD_FINGERPRINT := google/sailfish/sailfish:9/PQ2A.190205.003/5180536:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bootleggers.maintainer=shagbag913
