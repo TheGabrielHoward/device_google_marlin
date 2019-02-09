b3700335802bf4bb17906ca51c4dd2c03a150e5a
stebomurkn420 <stevn4127@gmail.com>
2019-02-09 09:56:01 -0500

    Use Dank Kernel

--- BoardConfigBootleg.mk
+++ BoardConfigBootleg.mk
@@ -1,12 +1,11 @@
 # Custom Bootleggers BoardConfig for marlin/sailfish
 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := bootleg_defconfig
+TARGET_KERNEL_CONFIG := dank_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := 6.0.2
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
