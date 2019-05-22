dacc96c2ab04d6f4cd647db84f7fed66a65b78bf
Tao Bao <tbao@google.com>
2019-05-21 20:51:10 -0700

    Deprecate PRODUCT_STATIC_BOOT_CONTROL_HAL.
    In favor of using boot control HAL under recovery.
    
    Bug: 34254109
    Test: TreeHugger
    Test: Build and flash sailfish. Do sanity check.
    Test: Sideloading a package on sailfish.
    Change-Id: I01e5f605749f5d2ffa58d10b1250659ff4db1d59

--- device-common.mk
+++ device-common.mk
@@ -486,17 +486,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     update_engine \
-    update_verifier
-
-# Enable update engine sideloading by including the static version of the
-# boot_control HAL and its dependencies.
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.msm8996 \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
+    update_verifier \
     update_engine_sideload
 
 PRODUCT_PACKAGES_DEBUG += \
@@ -583,6 +573,7 @@ PRODUCT_PACKAGES += \
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
+    android.hardware.boot@1.0-impl.recovery:64 \
     android.hardware.boot@1.0-service
 
 # Library used for VTS tests
@@ -612,7 +603,8 @@ PRODUCT_COPY_FILES += \
 
 # Bootloader HAL used for A/B updates.
 PRODUCT_PACKAGES += \
-    bootctrl.msm8996
+    bootctrl.msm8996 \
+    bootctrl.msm8996.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
