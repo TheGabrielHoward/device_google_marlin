c5d720b093a17af06f9d485fe810dd9829a1dfd1
shagbag913 <sh4gbag913@gmail.com>
2019-09-07 17:05:12 -0400

    Add Lineage build targets
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

---
+++ lineage_sailfish.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit Lineage product configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/marlin/aosp_sailfish.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_sailfish
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel
+TARGET_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sailfish \
+    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.190711.020 5800535 release-keys"
+
+BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.190711.020/5800535:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/sailfish/sailfish-vendor.mk)
