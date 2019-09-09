dfc5f628996e4560376c00f4f3d81dfd35d20e3e
dianlujitao <dianlujitao@lineageos.org>
2019-09-09 15:04:49 +0800

    marlin/sailfish: Remove custom kernel cross compile prefix
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,7 +122,6 @@ endif
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_CONFIG := marlin_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_SOURCE := kernel/google/marlin
