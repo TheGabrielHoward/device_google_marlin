ff7309c1566f3b42a69a0550a8eb67ed94f22059
thegabrielhoward <j0s3ghoward@gmail.com>
2019-10-12 10:25:25 +0000

    marlin & sailfish: Set TARGET_KERNEL_CLANG_VERSION to 9.0.5

--- BoardConfigLiquid.mk
+++ BoardConfigLiquid.mk
@@ -6,7 +6,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)
+TARGET_KERNEL_CLANG_VERSION := 9.0.5
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
