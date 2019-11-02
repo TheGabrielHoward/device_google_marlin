856d6f168e04910e37b1cd087b7474dda8976f07
thegabrielhoward <j0s3ghoward@gmail.com>
2019-11-02 18:57:31 +0000

    Update full_marlin.mk

--- full_marlin.mk
+++ full_marlin.mk
@@ -14,12 +14,6 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A1 5908163 release-keys"
-
-BUILD_FINGERPRINT := google/marlin/marlin:10/QP1A.191005.007.A1/5908163:user/release-keys
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
--- full_sailfish.mk
+++ full_sailfish.mk
@@ -14,12 +14,6 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A1 5908163 release-keys"
-
-BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
