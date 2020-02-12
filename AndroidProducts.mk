7f192a0a72d1dbf934e17e5b9b33fb934ab57b72
shagbag913 <sh4gbag913@gmail.com>
2020-02-11 21:03:27 -0500

    Add GahsROM product targets for marlin/sailfish
    Change-Id: I6cd6d42c6e1f95f795910a49312a8bb3ba9f89d1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,14 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk
 
+PRODUCT_MAKEFILES += \
+	$(LOCAL_DIR)/gahs_marlin.mk \
+	$(LOCAL_DIR)/gahs_sailfish.mk
+
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
 	aosp_sailfish-userdebug
+
+COMMON_LUNCH_CHOICES += \
+	gahs_sailfish-userdebug \
+	gahs_marlin-userdebug
