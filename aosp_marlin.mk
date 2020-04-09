466ed263db11e47bcb27b183f2d40ef9599f482a
Gabriel Howard <j0s3ghoward@gmail.com>
2020-04-09 13:09:03 +0200

    marlin & sailfish: Don't restrict vendor files

--- aosp_marlin.mk
+++ aosp_marlin.mk
@@ -30,7 +30,7 @@ PRODUCT_DEVICE := marlin
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 PRODUCT_MANUFACTURER := Google
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_COPY_FILES += \
     device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.marlin
